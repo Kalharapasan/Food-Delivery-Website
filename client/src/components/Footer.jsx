9363c28e9bd36c208f1e8bcdbc246941cf2ce9ce
Pasan Kalhara <kalharap84@gmail.com>
2026-05-20 23:38:47 +0530

    Add social icons to Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { assets } from '../../assets/assets'
 const Footer = () => {
     return (
         <footer className='footer' id='footer'>
+           
             <div className="footer-grid">
                 <div className="footer-brand">
                     <div className="logo-row">
@@ -15,9 +16,14 @@ const Footer = () => {
                     </p>
                 </div>
                 <div className="footer-social">
-                    
+                    <div className="social-icon" title="Facebook">𝗙</div>
+                    <div className="social-icon" title="Twitter">𝕏</div>
+                    <div className="social-icon" title="Instagram">◈</div>
+                    <div className="social-icon" title="LinkedIn">in</div>
                 </div>
             </div>
+
+            
         </footer>
     )
 }
