d31e8a4dec7ce56ef8103a4071a494d4ebb87b15
Pasan Kalhara <kalharap84@gmail.com>
2026-05-20 21:58:27 +0530

    Add Header component structure with image import

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,12 @@
 import React from 'react'
 import headerImage from '../../assets/header_im.jpg'
 
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
