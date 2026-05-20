e3d74db31471d6b978e7770d6cf7ba3b9cb879ea
Pasan Kalhara <kalharap84@gmail.com>
2026-05-21 01:19:59 +0530

    Add StarIcon component to Fooditem for visual representation

--- client/src/components/Fooditem.jsx
+++ client/src/components/Fooditem.jsx
@@ -1,10 +1,14 @@
 import React, { useContext, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
 
+const StarIcon = ({ filled }) => (
+  
+)
+
 const Fooditem = ({ id, name, price, description, image, category, isNew, isPopular }) => {
 
     return(
-        
+
     )
 
 }
