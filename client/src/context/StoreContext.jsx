7dee7183d5f549b2cb5d617d4e7d62b2bf14a255
Pasan Kalhara <kalharap84@gmail.com>
2026-05-20 17:00:19 +0530

    Add initial implementation of StoreContextProvider for state management

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -2,3 +2,8 @@ import React, { createContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
 export const StoreContext = createContext(null);
+
+const StoreContextProvider = ({ children }) => { 
+
+}
+export default StoreContextProvider;
