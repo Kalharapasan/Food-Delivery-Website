53c0cb5614133a295092bca54818c60988dc270f
Pasan Kalhara <kalharap84@gmail.com>
2026-05-21 01:30:32 +0530

    Add filtering logic to FoodDisplay component

--- client/src/components/FoodDisplay.jsx
+++ client/src/components/FoodDisplay.jsx
@@ -4,6 +4,9 @@ import Fooditem from '../Fooditem'
 
 const FoodDisplay = ({ category }) => {
     const { food_list, loading, searchQuery } = useContext(StoreContext)
+    const filteredList = food_list.filter(item => {
+        
+    })
     return (
 
     )
