dfad99f8c60e3d11dab360df7ee36851b1ce1cd7
Pasan Kalhara <kalharap84@gmail.com>
2026-05-20 23:20:53 +0530

    Refactor Navbar component to render navigation links dynamically

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,12 +6,26 @@ import { assets } from '../../assets/assets'
 const Navbar = ({ setShowLogin }) => {
     return (
         <nav className="navbar">
+            
             <Link to="/">
                 <img className="logo" src={assets.logo} alt="Logo" />
             </Link>
+           
             <ul className="navbar-menu">
-
+                {navLinks.map(link => (
+                    <li key={link.label}>
+                        {link.path ? (
+                            <Link to={link.path} className={location.pathname === link.path ? 'active' : ''}>
+                                {link.label}
+                            </Link>
+                        ) : (
+                            <a href={link.href}>{link.label}</a>
+                        )}
+                    </li>
+                ))}
             </ul>
+
+
         </nav>
     )
 }
