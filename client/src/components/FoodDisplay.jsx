5ec60d9d40994efc853c7deb4164c7ac0e04be68
Pasan Kalhara <kalharap84@gmail.com>
2026-05-21 01:40:37 +0530

    Add SkeletonCard component to FoodDisplay for loading state

--- client/src/components/FoodDisplay.jsx
+++ client/src/components/FoodDisplay.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import Fooditem from '../Fooditem'
 
+const SkeletonCard = () => (
+    
+)
+
 const FoodDisplay = ({ category }) => {
 
     const { food_list, loading, searchQuery } = useContext(StoreContext)
