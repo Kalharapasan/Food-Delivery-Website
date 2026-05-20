9e6bb9daf1d2739fb09afb50e2e64e364b05e98d
Pasan Kalhara <kalharap84@gmail.com>
2026-05-20 22:53:45 +0530

    Add header badge for promotional message in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,9 +4,12 @@ import headerImage from '../../assets/header_im.jpg'
 const Header = () => {
   return (
     <div className='header' style={{ backgroundImage: `url(${headerImage})` }}>
-      <div className="header-overlay" /> 
+      <div className="header-overlay" />
       <div className="header-contents">
-
+        <div className="header-badge">
+          🔥 Free delivery on first order
+        </div>
+        
       </div>
     </div>
   )
