7e20e2b532ddda6c074f2dc1eca82a4f457454b3
Pasan Kalhara <kalharap84@gmail.com>
2026-05-21 01:19:42 +0530

    Implement Fooditem component structure with props

--- client/src/components/Fooditem.jsx
+++ client/src/components/Fooditem.jsx
@@ -1,2 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
+
+const Fooditem = ({ id, name, price, description, image, category, isNew, isPopular }) => {
+
+    return(
+        
+    )
+
+}
+export default Fooditem
