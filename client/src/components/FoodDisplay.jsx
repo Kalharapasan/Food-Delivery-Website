623acf7aa626dc652474f6754c13695782513acc
Pasan Kalhara <kalharap84@gmail.com>
2026-05-21 01:29:52 +0530

    Add initial structure to FoodDisplay component

--- client/src/components/FoodDisplay.jsx
+++ client/src/components/FoodDisplay.jsx
@@ -2,3 +2,10 @@ import React, { useContext } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import Fooditem from '../Fooditem'
 
+const FoodDisplay = ({ category }) => {
+    return(
+
+    )
+}
+
+export default FoodDisplay
