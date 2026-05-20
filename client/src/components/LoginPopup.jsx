cd05320e936d63c2efe695b82ac6921f09866fb9
Pasan Kalhara <kalharap84@gmail.com>
2026-05-21 01:02:51 +0530

    Add MailIcon component to LoginPopup

--- client/src/components/LoginPopup.jsx
+++ client/src/components/LoginPopup.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
 
-
+const MailIcon = () => (
+  
+)
 
 const LoginPopup = ({ setShowLogin }) => {
     return ()
