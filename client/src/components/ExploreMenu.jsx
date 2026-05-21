27c53da21c82296a756f27466782b048576d63c8
Pasan Kalhara <kalharap84@gmail.com>
2026-05-22 01:26:48 +0530

    Add section header div to ExploreMenu component

--- client/src/components/ExploreMenu.jsx
+++ client/src/components/ExploreMenu.jsx
@@ -4,7 +4,9 @@ import { menu_list } from '../assets/assets'
 const ExploreMenu = ({ category, setCategory }) => {
     return (
         <div className='explore-menu' id='explore-menu'>
-            
+            <div className="section-header">
+                
+            </div>
         </div>
     )
 }
