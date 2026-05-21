68e2f66b75bd9aec7f009dac79831bede1f20ba2
Pasan Kalhara <kalharap84@gmail.com>
2026-05-22 01:36:26 +0530

    Refactor PlaceOrder component structure and formatting

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -2,9 +2,14 @@ import React, { useContext, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import axios from 'axios'
 
-const PlaceOrder = () => {
-    return(
 
+const PlaceOrder = () => {
+    
+    
+    return(
+        <div className="place-order">
+            
+        </div>
     )
 }
 
