3334fe791784ea345f1a7f3bc7b7bd8f9090c4ef
Pasan Kalhara <kalharap84@gmail.com>
2026-05-22 01:40:26 +0530

    Add empty form row for future input in Delivery Information section of PlaceOrder component

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -39,6 +39,10 @@ const PlaceOrder = () => {
                         </div>
                     </div>
 
+                    <div className="form-row">
+                        
+                    </div>
+
                 </div>
             </form>
 
