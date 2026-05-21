c36ae7e03b3c5ea10b5487f9e3ed3f2aa1f1745b
Pasan Kalhara <kalharap84@gmail.com>
2026-05-22 01:27:48 +0530

    Add explore menu list div structure to ExploreMenu component

--- client/src/components/ExploreMenu.jsx
+++ client/src/components/ExploreMenu.jsx
@@ -4,11 +4,17 @@ import { menu_list } from '../assets/assets'
 const ExploreMenu = ({ category, setCategory }) => {
     return (
         <div className='explore-menu' id='explore-menu'>
+            
             <div className="section-header">
                 <div className="section-tag">Categories</div>
                 <h2>Explore our menu</h2>
                 <p>Choose from a diverse menu featuring a delectable array of dishes.</p>
             </div>
+
+            <div className="explore-menu-list">
+                
+            </div>
+
         </div>
     )
 }
