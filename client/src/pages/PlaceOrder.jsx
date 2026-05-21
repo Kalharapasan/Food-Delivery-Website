24e715202bb87c0d4daf15199c2bba2740a4b1a1
Pasan Kalhara <kalharap84@gmail.com>
2026-05-22 01:35:52 +0530

    Add initial structure for PlaceOrder component

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -1,3 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import axios from 'axios'
+
+const PlaceOrder = () => {
+    return(
+
+    )
+}
+
+export default PlaceOrder
