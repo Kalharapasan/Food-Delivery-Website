5dd83d2d8f76124ba182a028a4ae0265f1e08074
Pasan Kalhara <kalharap84@gmail.com>
2026-05-22 01:26:33 +0530

    Add div structure to ExploreMenu component

--- client/src/components/ExploreMenu.jsx
+++ client/src/components/ExploreMenu.jsx
@@ -3,7 +3,9 @@ import { menu_list } from '../assets/assets'
 
 const ExploreMenu = ({ category, setCategory }) => {
     return (
-
+        <div className='explore-menu' id='explore-menu'>
+            
+        </div>
     )
 }
 
