ff67232a1bfe3365f741fd2203a9155a730fd30b
Pasan Kalhara <kalharap84@gmail.com>
2026-05-23 01:22:24 +0530

    Refactor MyOrders component: remove unused imports and simplify structure

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,20 +1,7 @@
-import React, { useState } from 'react'
-import Header from '../components/Header'
-import ExploreMenu from '../components/ExploreMenu'
-import FoodDisplay from '../components/FoodDisplay'
-import AppDownload from '../components/AppDownload'
+import React from 'react'
 
-const Home = () => {
-    const [category, setCategory] = useState("All")
-
-    return (
-        <div>
-            <Header />
-            <ExploreMenu category={category} setCategory={setCategory} />
-            <FoodDisplay category={category} />
-            <AppDownload />
-        </div>
-    )
+export const MyOrders = () => {
+  return (
+    <div>MyOrders</div>
+  )
 }
-
-export default Home
