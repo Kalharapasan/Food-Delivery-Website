3850fb53d23c27cab1c9bafb8f8f51213de310a6
Pasan Kalhara <kalharap84@gmail.com>
2026-05-23 00:56:41 +0530

    Add cart item display in Cart component

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,11 +3,13 @@ import { StoreContext } from '../context/StoreContext'
 import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
-   
-    return(
-         <div className="cart">
-            
-         </div>
+
+    return (
+        <div className="cart">
+            <h1 className="cart-page-title">Your Cart</h1>
+            <p className="cart-subtitle">{cartItems.length} item{cartItems.length !== 1 ? 's' : ''} in your cart</p>
+
+        </div>
     )
 
 }
