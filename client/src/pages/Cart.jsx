2d8eb354417f6ded23767a66bf85daec85f37baf
Pasan Kalhara <kalharap84@gmail.com>
2026-05-23 00:55:50 +0530

    Add Cart component structure with initial return statement

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,3 +2,10 @@ import React, { useContext, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import { useNavigate } from 'react-router-dom'
 
+const Cart = () => {
+    return(
+
+    )
+}
+
+export default Cart
