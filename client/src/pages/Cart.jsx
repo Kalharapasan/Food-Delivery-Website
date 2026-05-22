7e9c9b56296209c0566574ad09a14b0e31588a6c
Pasan Kalhara <kalharap84@gmail.com>
2026-05-23 01:13:20 +0530

    Update Cart component: handle invalid promo codes in applyPromo function

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -27,7 +27,9 @@ const Cart = () => {
             setPromoSuccess(`🎉 ${PROMO_CODES[code]}% discount applied!`)
             setPromoError('')
         } else {
-
+            setPromoError('Invalid promo code')
+            setPromoSuccess('')
+            setDiscount(0)
         }
     }
 
