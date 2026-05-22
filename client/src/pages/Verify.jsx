011e0ba8fc59a543301fc367d823a644066ccbf5
Pasan Kalhara <kalharap84@gmail.com>
2026-05-23 01:21:55 +0530

    Update Verify component: add component structure and export

--- client/src/pages/Verify.jsx
+++ client/src/pages/Verify.jsx
@@ -2,3 +2,14 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { StoreContext } from '../context/StoreContext'
 import axios from 'axios'
+
+const Verify = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Verify
