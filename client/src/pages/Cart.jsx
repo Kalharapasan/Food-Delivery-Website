6610ea3978a48beded44e5f1e49c8fbe8513bfd3
Pasan Kalhara <kalharap84@gmail.com>
2026-05-23 00:56:20 +0530

    Refactor Cart component: format code for consistency and improve readability

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,9 +3,13 @@ import { StoreContext } from '../context/StoreContext'
 import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
+   
     return(
-
+         <div className="cart">
+            
+         </div>
     )
+
 }
 
 export default Cart
