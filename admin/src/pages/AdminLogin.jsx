e01fe2778deb91af4a7b7fb75ccbdb98e6c1f456
Pasan Kalhara <kalharap84@gmail.com>
2026-05-24 01:49:18 +0530

    feat: initialize state management in AdminLogin component

--- admin/src/pages/AdminLogin.jsx
+++ admin/src/pages/AdminLogin.jsx
@@ -4,6 +4,11 @@ import { toast } from 'react-toastify'
 import './AdminLogin.css'
 
 const AdminLogin = () => {
+
+  const [currState, setCurrState] = useState("Sign In")
+  const [data, setData] = useState({ name: '', email: '', password: '' })
+  const [loading, setLoading] = useState(false)
+
   return (
     <div>AdminLogin</div>
   )
