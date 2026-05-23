77faef0916a7392bb7e2f40d37be42d8e6283da4
Pasan Kalhara <kalharap84@gmail.com>
2026-05-24 01:40:56 +0530

    feat: initialize Sidebar component with imports and structure

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      
+    </div>
   )
 }
 
