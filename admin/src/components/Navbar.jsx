e2d9b1ddbb37b3967e0c337a305bb22f1bbe6cae
Pasan Kalhara <kalharap84@gmail.com>
2026-05-24 01:38:32 +0530

    feat: add admin badge and logout button to Navbar component

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -8,7 +8,13 @@ const Navbar = () => {
       <div className='navbar'>
         <img className='logo' src={assets.logo} alt="Logo" />
         <div className='navbar-right'>
-          
+          <span className='admin-badge'>Admin</span>
+          {logout && (
+            <button className='logout-btn' onClick={logout}>
+              Logout
+            </button>
+          )}
+          <img className='profile' src={assets.profile_image} alt="Profile" />
         </div>
       </div>
     </div>
