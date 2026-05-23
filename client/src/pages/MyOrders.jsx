6d131f1dabef84645037943321c866087bf3bbcb
Pasan Kalhara <kalharap84@gmail.com>
2026-05-24 00:57:49 +0530

    Update MyOrders component: define STATUS_STEPS for order status tracking

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -4,6 +4,8 @@ import { assets } from '../assets'
 import axios from 'axios'
 
 
+const STATUS_STEPS = ['Food Processing', 'Out for Delivery', 'Delivered']
+
 export const MyOrders = () => {
   return (
     <div>MyOrders</div>
