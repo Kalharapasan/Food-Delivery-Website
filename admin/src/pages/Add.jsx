9e6943dd310d0316134f3b68c5f8b654af6fe9e2
Pasan Kalhara <kalharap84@gmail.com>
2026-05-25 00:58:46 +0530

    feat: restructure Add component header for improved readability

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Add = () => {
   return (
     <div className='add'>
-      
-      <div className='add-header'>
 
+      <div className='add-header'>
+        <h2>Add New Food Item</h2>
+        <p>Fill in the details to add a new item to your menu</p>
       </div>
 
-      
+
     </div>
   )
 }
