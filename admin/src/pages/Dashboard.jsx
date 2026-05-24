b1053fe8453b078295c5699315c98e310727bf25
Pasan Kalhara <kalharap84@gmail.com>
2026-05-25 00:44:01 +0530

    feat: add initial imports and styles to Dashboard component

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { assets } from '../assets/assets'
+import './Dashboard.css'
+
 
 const Dashboard = () => {
   return (
