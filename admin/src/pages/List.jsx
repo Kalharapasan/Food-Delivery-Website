266fce53989b884b47a7b0efea6ad9d94da88898
Pasan Kalhara <kalharap84@gmail.com>
2026-05-26 01:29:32 +0530

    feat: update List component to include axios, toast notifications, and CSS styling

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import './List.css'
 
 const List = () => {
   return (
-    <div>List</div>
+    <div>
+      
+    </div>
   )
 }
 
