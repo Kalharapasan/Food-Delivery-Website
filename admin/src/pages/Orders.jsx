b2f03b7317fcabb54db9957a7fd9ad9150f54ce7
Pasan Kalhara <kalharap84@gmail.com>
2026-05-26 01:48:44 +0530

    feat: enhance Orders component with necessary imports and styles

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import { assets } from '../assets/assets'
+import './Orders.css'
 
 const Orders = () => {
   return (
