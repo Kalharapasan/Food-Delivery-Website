fa1c986f1ec203fc297af525c88254413978e759
Pasan Kalhara <kalharap84@gmail.com>
2026-05-26 01:30:37 +0530

    feat: refine List component header structure and display item count

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -4,13 +4,15 @@ import { toast } from 'react-toastify'
 import './List.css'
 
 const List = () => {
+  
   return (
     <div className='list'>
-      
+
       <div className='list-header'>
-         <div>
-          
-         </div>
+        <div>
+          <h2>Food Items</h2>
+          <p>{list.length} items on menu</p>
+        </div>
       </div>
 
 
