ff9c8a5618416ae53a19512c2f2968853a670518
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 23:31:51 +0530

    feat: return token in createToken function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,6 +7,7 @@ const createToken = (id) => {
     if (!process.env.JWT_SECRET) {
         throw new Error("JWT_SECRET is not defined in environment variables");
     }
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 
