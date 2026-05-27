328100c7a99b3f009a8333f3bc1a6a4c83335c74
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 20:00:24 +0530

    feat: adjust formatting and ensure webhook middleware is correctly placed in server.js

--- server/server.js
+++ server/server.js
@@ -9,14 +9,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+// Webhook MUST be before express.json() middleware
+app.post(
+    "/api/order/webhook",
+    express.raw({ type: "application/json" }),
+    stripeWebhook
+);
 
 // DB connection
 connectDB();
 
 app.get("/", (req, res) => {
-  res.send("API Working ✅");
+    res.send("API Working ✅");
 });
 
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on http://localhost:${port}`);
 });
