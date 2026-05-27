a1bdd6c1b50a25d555c7beb0b52821df512173e2
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 23:29:26 +0530

    feat: implement createToken function in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,3 +2,7 @@ import { supabase } from "../config/Databases.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
+
+const createToken = (id) => {
+
+};
