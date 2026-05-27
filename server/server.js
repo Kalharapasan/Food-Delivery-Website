c5a336c2bca34deee48583cc4b9da6687c155aae
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 19:45:26 +0530

    feat: add API endpoint for root path and start server

--- server/server.js
+++ server/server.js
@@ -8,3 +8,12 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+
+app.get("/", (req, res) => {
+  res.send("API Working ✅");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
