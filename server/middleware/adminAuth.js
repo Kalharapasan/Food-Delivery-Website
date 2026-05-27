56d9048421878be5278b7bba3d3a8b27d5018acf
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 19:55:54 +0530

    feat: add token validation in adminAuth middleware

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -3,6 +3,9 @@ import { supabase } from "../config/Databases.js";
 
 const adminAuth = async (req, res, next) => {
     const { token } = req.headers;
+    if (!token) {
+        return res.json({ success: false, message: "Not authorized. Admin login required." });
+    }
 
 };
 
