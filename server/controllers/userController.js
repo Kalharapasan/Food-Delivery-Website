4d7dd9db230e68f72f9b2acbcacde1a6e5a70def
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 23:31:37 +0530

    feat: implement JWT_SECRET check in createToken function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,9 @@ import bcrypt from "bcrypt";
 import validator from "validator";
 
 const createToken = (id) => {
-    if (!process.env.JWT_SECRET) {}
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
 };
 
 
