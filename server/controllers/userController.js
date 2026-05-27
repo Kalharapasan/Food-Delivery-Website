a7763b4e62961125d86f3120c47926fd7a638cce
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 23:39:12 +0530

    feat: implement initial structure for registerUser function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -39,7 +39,14 @@ const loginUser = async (req, res) => {
     }
 };
 
-const registerUser = async (req, res) => { };
+const registerUser = async (req, res) => { 
+    const { name, password, email } = req.body;
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
 const adminLogin = async (req, res) => { };
 
