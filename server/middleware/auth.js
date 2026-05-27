23d3d55e8cdf3f7f4df0ced736ae23717791a388
Pasan Kalhara <kalharap84@gmail.com>
2026-05-27 19:53:14 +0530

    feat: complete token verification logic in auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,9 +8,11 @@ const authMiddleware = async (req, res, next) => {
     }
 
     try {
-        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = decoded.id;
+        next();
     } catch (error) {
-        
+
     }
 
 }
