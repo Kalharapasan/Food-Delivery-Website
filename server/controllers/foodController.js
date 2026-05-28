039a858e53f0d974ac258233a8763720b212136b
Pasan Kalhara <kalharap84@gmail.com>
2026-05-28 22:51:55 +0530

    fix: add file check and error response in addFood function in foodController

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -2,7 +2,9 @@ import { supabase } from "../config/Databases.js";
 import fs from "fs";
 
 const addFood = async (req, res) => {
-    if (!req.file) {}
+    if (!req.file) {
+        return res.status(400).json({ success: false, message: "Image file is required" });
+    }
 }
 
 const listFood = async (req, res) => {}
