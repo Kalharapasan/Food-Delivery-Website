225eb47f64b6bd0c3b79447b88ecb20af4d3d26c
Pasan Kalhara <kalharap84@gmail.com>
2026-05-28 22:48:11 +0530

    fix: add userRouter initialization in userRoute

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,2 +1,7 @@
 import express from "express";
 import { loginUser,registerUser,adminLogin, adminRegister } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+
+export default userRouter;
