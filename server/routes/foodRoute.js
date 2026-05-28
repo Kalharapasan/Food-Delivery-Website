b93f314bce512f7c37003df2f31525c1ee7d8cc8
Pasan Kalhara <kalharap84@gmail.com>
2026-05-28 22:59:00 +0530

    fix: add necessary imports for foodRouter functionality in foodRoute

--- server/routes/foodRoute.js
+++ server/routes/foodRoute.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { addFood, listFood, removeFood, updateFood } from "../controllers/foodController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import multer from "multer";
 
 const foodRouter = express.Router();
 
