5318186510ba140217a90d78a8283c6739aaa8b6
Pasan Kalhara <kalharap84@gmail.com>
2026-05-28 22:51:22 +0530

    fix: implement listFood, removeFood, and updateFood function definitions in foodController

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -2,3 +2,13 @@ import { supabase } from "../config/Databases.js";
 import fs from "fs";
 
 const addFood = async (req, res) => {}
+
+const listFood = async (req, res) => {}
+
+const removeFood = async (req, res) => {}
+
+
+const updateFood = async (req, res) => {}
+
+
+export { addFood, listFood, removeFood, updateFood };
