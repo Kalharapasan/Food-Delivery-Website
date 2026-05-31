b9d5a238cd1c6cdaa997fd309ef2d487bdf24a6f
Pasan Kalhara <kalharap84@gmail.com>
2026-05-31 16:56:06 +0530

    feat: implement cartController with addToCart, removeFromCart, and getCart functions

---
+++ server/controllers/cartController.js
@@ -0,0 +1,14 @@
+import { supabase } from "../config/Databases.js";
+
+const addToCart = async (req, res) => {
+};
+
+const removeFromCart = async (req, res) => {
+
+};
+
+const getCart = async (req, res) => {
+
+};
+
+export { addToCart, removeFromCart, getCart }
