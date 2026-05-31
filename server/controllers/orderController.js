bef7cf83a657813e4b0012c8e13b5a981388948a
Pasan Kalhara <kalharap84@gmail.com>
2026-05-31 16:46:52 +0530

    fix: enhance placeOrder function to include order insertion logic

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,23 +6,40 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req, res) => {
     const frontend_url = process.env.FRONTEND_URL;
     try {
+        const { data: newOrder, error } = await supabase
+            .from("orders")
+            .insert([
+                {
+                    userId: req.body.userId,
+                    items: req.body.items,
+                    amount: req.body.amount,
+                    address: req.body.address,
+                    date: new Date().toISOString(),
+                    payment: false,
+                    status: "Food Processing",
+                },
+            ])
+            .select()
+            .single();
+        
+        
         
     } catch (error) {
-        
+
     }
 };
 
-const verifyOrder = async (req, res) => {};
+const verifyOrder = async (req, res) => { };
 
-const userOrders = async (req, res) => {};
+const userOrders = async (req, res) => { };
 
-const listOrders = async (req, res) => {};
+const listOrders = async (req, res) => { };
 
-const updateStatus = async (req, res) => {};
+const updateStatus = async (req, res) => { };
 
-const getSummary = async (req, res) => {};
+const getSummary = async (req, res) => { };
 
-const stripeWebhook = async (request, response) => {};
+const stripeWebhook = async (request, response) => { };
 
 
 export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus, getSummary, stripeWebhook };
