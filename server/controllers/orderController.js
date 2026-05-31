7ce0929f07fba1973c7b91b52c59148546bfc0a1
Pasan Kalhara <kalharap84@gmail.com>
2026-05-31 16:45:20 +0530

    feat: implement order controller with initial order handling functions

---
+++ server/controllers/orderController.js
@@ -0,0 +1,19 @@
+import { supabase } from "../config/Databases.js";
+import Stripe from "stripe";
+
+const placeOrder = async (req, res) => {};
+
+const verifyOrder = async (req, res) => {};
+
+const userOrders = async (req, res) => {};
+
+const listOrders = async (req, res) => {};
+
+const updateStatus = async (req, res) => {};
+
+const getSummary = async (req, res) => {};
+
+const stripeWebhook = async (request, response) => {};
+
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus, getSummary, stripeWebhook };
